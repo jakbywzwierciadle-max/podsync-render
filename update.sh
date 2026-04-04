c2be38a44f42bbabbe10056a400967be6bab4929
jakbywzwierciadle-max <jakbywzwierciadle@gmail.com>
2026-04-04 18:58:52 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -13,22 +13,20 @@ while IFS= read -r URL; do
 
     echo "Pobieram z: $URL"
 
-  yt-dlp \
-    --cookies "$COOKIES_FILE" \
-    --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0 Safari/537.36" \
-    --extractor-args "youtube:player_client=web,web_creator,android,ios,tv"
-    --force-ipv4 \
-    -f "bestaudio* / bestaudio / best / 18 / 22" \
-    --extract-audio \
-    --audio-format mp3 \
-    --audio-quality 0 \
-    --playlist-end 1 \
-    --no-warnings \
-    --ignore-errors \
-    --output "$DATA_DIR/%(upload_date)s-%(title)s.%(ext)s" \
-    "$URL"
-
-
+    yt-dlp \
+        --cookies "$COOKIES_FILE" \
+        --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0 Safari/537.36" \
+        --extractor-args "youtube:player_client=web,web_creator,android,ios,tv" \
+        --force-ipv4 \
+        -f "bestaudio* / bestaudio / best / 18 / 22" \
+        --extract-audio \
+        --audio-format mp3 \
+        --audio-quality 0 \
+        --playlist-end 1 \
+        --no-warnings \
+        --ignore-errors \
+        --output "$DATA_DIR/%(upload_date)s-%(title)s.%(ext)s" \
+        "$URL"
 
 done < "$CHANNELS_FILE"
 
