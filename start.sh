f8c4ea4558fe04069ca5218cad6baed1b0e98122
jakbywzwierciadle-max <jakbywzwierciadle@gmail.com>
2026-04-04 13:38:38 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
-PLAYLIST_URL="https://www.youtube.com/channel/UCO6_hwMtQZ0SLElfDMaqJGQ"
+PLAYLIST_URL="https://www.youtube.com/playlist?list=UUO6_hwMtQZ0SLElfDMaqJGQ"
 
 mkdir -p /data
 
-# uruchom RSS server w tle
+echo "=== Start dir2cast ==="
 dir2cast /data --port 3000 &
 
-# loop
 while true; do
   echo "=== Pobieranie ==="
 
-  yt-dlp -f "bestaudio[language=pl]" \
+  yt-dlp \
+    --cookies /app/cookies.txt \
+    --sleep-requests 2 \
+    --sleep-interval 2 \
+    --max-sleep-interval 5 \
+    -f "bestaudio[language=pl]" \
     -x --audio-format mp3 \
     --download-archive /data/archive.txt \
     -o "/data/%(upload_date)s-%(title)s.%(ext)s" \
     "$PLAYLIST_URL"
 
-  echo "=== Czyszczenie ==="
-
+  echo "=== Czyszczenie (zostaw 2) ==="
   ls -t /data/*.mp3 2>/dev/null | tail -n +3 | xargs -r rm --
 
-  echo "=== Sleep ==="
+  echo "=== Sleep 1h ==="
   sleep 3600
 done
