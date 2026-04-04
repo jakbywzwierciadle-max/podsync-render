5cff762318be60365fdf63b2fab69761e9b48255
jakbywzwierciadle-max <jakbywzwierciadle@gmail.com>
2026-04-04 17:45:49 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,29 +1,7 @@
 #!/bin/bash
 
-PLAYLIST_URL="https://www.youtube.com/playlist?list=UUO6_hwMtQZ0SLElfDMaqJGQ"
+# Uruchamiamy aktualizację w tle
+/app/update.sh &
 
-mkdir -p /data
-
-echo "=== Start RSS server ==="
-python app.py &
-
-while true; do
-  echo "=== Pobieranie ==="
-
-  yt-dlp \
-    --cookies /app/cookies.txt \
-    --sleep-requests 2 \
-    --sleep-interval 2 \
-    --max-sleep-interval 5 \
-    -f "bestaudio[language=pl]" \
-    -x --audio-format mp3 \
-    --download-archive /data/archive.txt \
-    -o "/data/%(upload_date)s-%(title)s.%(ext)s" \
-    "$PLAYLIST_URL"
-
-  echo "=== Czyszczenie (2 najnowsze) ==="
-  ls -t /data/*.mp3 2>/dev/null | tail -n +3 | xargs -r rm --
-
-  echo "=== Sleep 1h ==="
-  sleep 3600
-done
+# Uruchamiamy serwer Flask
+python /app/app.py
