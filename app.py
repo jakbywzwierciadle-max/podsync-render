49a55bfd81ba9991675cc37befda3daad0dd5223
jakbywzwierciadle-max <jakbywzwierciadle@gmail.com>
2026-04-04 14:26:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,44 +1,20 @@
-from flask import Flask, Response, send_from_directory
-from feedgen.feed import FeedGenerator
+from flask import Flask, send_from_directory
 import os
 
 app = Flask(__name__)
 DATA_DIR = "/data"
 
-@app.route("/")
-def index():
-    return "RSS działa 🚀"
+@app.route("/feed.xml")
+def feed():
+    return send_from_directory(DATA_DIR, "feed.xml", mimetype="application/rss+xml")
 
-@app.route("/rss.xml")
-def rss():
-    fg = FeedGenerator()
-    fg.title("YouTube Podcast")
-    fg.link(href="http://localhost:3000/rss.xml", rel="self")
-    fg.description("Auto RSS z YouTube (PL audio)")
-
-    files = sorted(
-        [f for f in os.listdir(DATA_DIR) if f.endswith(".mp3")],
-        reverse=True
-    )
-
-    for file in files:
-        fe = fg.add_entry()
-        fe.title(file)
-
-        url = f"{get_base_url()}/audio/{file}"
-        fe.enclosure(url, 0, "audio/mpeg")
-
-    return Response(fg.rss_str(pretty=True), mimetype="application/rss+xml")
-
-
-@app.route("/audio/<path:filename>")
-def audio(filename):
+@app.route("/<path:filename>")
+def files(filename):
     return send_from_directory(DATA_DIR, filename)
 
-
-def get_base_url():
-    return "https://" + os.environ.get("RAILWAY_STATIC_URL", "localhost:3000")
-
+@app.route("/")
+def index():
+    return "RSS: /feed.xml\n"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
