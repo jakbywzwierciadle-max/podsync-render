9fb31fef8538cd561792693b9097272254107e36
jakbywzwierciadle-max <jakbywzwierciadle@gmail.com>
2026-04-06 12:26:08 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -13,11 +13,12 @@ while IFS= read -r URL; do
 
     echo "Pobieram z: $URL"
 
-    yt-dlp \
+
+        yt-dlp \
         --cookies "$COOKIES_FILE" \
         --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0 Safari/537.36" \
         --force-ipv4 \
-        -f "ba/b" \
+        -f "bestaudio[ext=m4a]/bestaudio/best" \
         --extract-audio \
         --audio-format mp3 \
         --audio-quality 0 \
@@ -25,8 +26,10 @@ while IFS= read -r URL; do
         --match-filter "!is_live & !was_live & duration > 30" \
         --no-warnings \
         --ignore-errors \
+        --add-header "Accept-Language:en-US,en;q=0.9" \
         --output "$DATA_DIR/%(upload_date)s-%(title)s.%(ext)s" \
         "$URL"
+    
 
 done < "$CHANNELS_FILE"
 
