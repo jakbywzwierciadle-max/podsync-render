e755de6a477d5f081090c667463b5a6b58edb613
jakbywzwierciadle-max <jakbywzwierciadle@gmail.com>
2026-04-06 11:16:44 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -16,7 +16,6 @@ while IFS= read -r URL; do
     yt-dlp \
         --cookies "$COOKIES_FILE" \
         --user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0 Safari/537.36" \
-        --extractor-args "youtube:player_client=web,web_creator,android,ios,tv" \
         --force-ipv4 \
         -f "ba/b" \
         --extract-audio \
